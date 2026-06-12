a9afb26ab418224bbddec52d47f5b496cc6c8b2a
richie-rich90454 <rjiang880@gmail.com>
2026-06-12 12:32:24 +0800

    feat: enable async support in OpensolutionsApplication

--- backend/opensolutions/src/main/java/com/richardjiang880/opensolutions/OpensolutionsApplication.java
+++ backend/opensolutions/src/main/java/com/richardjiang880/opensolutions/OpensolutionsApplication.java
@@ -2,8 +2,10 @@ package com.richardjiang880.opensolutions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class OpensolutionsApplication {
 
 	public static void main(String[] args) {
