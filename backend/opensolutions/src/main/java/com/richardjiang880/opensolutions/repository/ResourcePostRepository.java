d24c171e5d1641ece9d3b0e77caae4528564f8c8
richie-rich90454 <rjiang880@gmail.com>
2026-06-12 12:32:58 +0800

    feat: add ResourcePostRepository for resource posts

---
+++ backend/opensolutions/src/main/java/com/richardjiang880/opensolutions/repository/ResourcePostRepository.java
@@ -0,0 +1,13 @@
+package com.richardjiang880.opensolutions.repository;
+
+import com.richardjiang880.opensolutions.model.ResourcePost;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ResourcePostRepository extends JpaRepository<ResourcePost, Long> {
+
+    Page<ResourcePost> findByThreadIdOrderByCreatedAtAsc(Long threadId, Pageable pageable);
+}
