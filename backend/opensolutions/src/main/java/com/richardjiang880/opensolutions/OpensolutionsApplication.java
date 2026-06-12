35bca1f2cb7310db9bce111cdcaf30ff7196eff7
richie-rich90454 <rjiang880@gmail.com>
2026-06-12 11:37:59 +0800

    chore: initialize project with Spring Boot skeleton

---
+++ backend/opensolutions/src/main/java/com/richardjiang880/opensolutions/OpensolutionsApplication.java
@@ -0,0 +1,13 @@
+package com.richardjiang880.opensolutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpensolutionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpensolutionsApplication.class, args);
+	}
+
+}
---
+++ backend/opensolutions/src/test/java/com/richardjiang880/opensolutions/OpensolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.richardjiang880.opensolutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpensolutionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
