db4b0b596ac03a8685d0e4d6d9ebbb56eafcd45f
richie-rich90454 <rjiang880@gmail.com>
2026-06-12 12:32:54 +0800

    feat: add SubjectRepository for subject data access

---
+++ backend/opensolutions/src/main/java/com/richardjiang880/opensolutions/repository/SubjectRepository.java
@@ -0,0 +1,9 @@
+package com.richardjiang880.opensolutions.repository;
+
+import com.richardjiang880.opensolutions.model.Subject;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SubjectRepository extends JpaRepository<Subject, Long> {
+}
